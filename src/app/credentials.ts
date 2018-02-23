ab29a5c07e5a554043ddfe90e41ca4c69a193409
Subramanyam Gurunath <papudi@gmail.com>
2018-02-23 16:39:22 +0530

    Delete credentials.ts

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-export const firebaseConfig = {
-    apiKey: "AIzaSyBci0AQtRTFdsorAhCRBIU8dmYF07baQM0",
-    authDomain: "rewardsmultiplier-kenwaves.firebaseapp.com",
-    databaseURL: "https://rewardsmultiplier-kenwaves.firebaseio.com",
-    projectId: "rewardsmultiplier-kenwaves",
-    storageBucket: "rewardsmultiplier-kenwaves.appspot.com",
-    messagingSenderId: "259556700295"
-};
